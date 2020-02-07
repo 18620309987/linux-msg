6e5f40783d0971ccad5eec8f6d9cab424a054ec3
tu98tu <tu98tu@163.com>
2020-02-07 20:46:19 +0800

    add client-copy.c by tu98tu

---
+++ client-copy.c
@@ -0,0 +1,29 @@
+//client-copy.c by tu98tu
+#include "comm.h"
+
+int main()
+{
+    int msgid = getMsgQueue();
+
+    char buf[1024] = {0};
+    while(1)
+    {
+        printf("Please Enter# ");
+        fflush(stdout);
+        
+		//注示：读取 by lijinting
+        ssize_t s = read(0, buf, sizeof(buf));
+        if(s > 0)
+        {
+            buf[s-1]=0;
+            sendMsgQueue(msgid, CLIENT_TYPE, buf);
+            if(strcasecmp("quit", buf) == 0)
+                break;
+            printf("send done, wait recv...\n");
+        }
+
+        recvMsgQueue(msgid, SERVER_TYPE, buf);
+        printf("server# %s\n", buf);
+    }
+    return 0;
+}
